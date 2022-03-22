f281f0b7a2e0a33b995a2787b9558d49f4ff2e18
Mark Wiggans <mmw125@gmail.com>
2022-03-21 20:14:14 -0400

    update resume and cubecobra to https

---
+++ decks.js
@@ -0,0 +1,361 @@
+var decks = {};
+decks['Basri'] = `
+1 Basri's Acolyte
+1 Basri's Lieutenant
+1 Concordia Pegasus
+1 Knight of the Tusk
+1 Siege Striker
+1 Staunch Shieldmate
+1 Tempered Veteran
+1 Basri Ket
+1 Feat of Resistance
+1 Basri's Solidarity
+1 Legion's Judgment
+1 Faith's Fetters
+1 Thriving Heath
+7 Plains`
+decks['Unicorns'] = `
+1 Brightmare
+1 Daybreak Charger
+1 Emiel the Blessed
+1 Inspiring Unicorn
+1 Mesa Unicorn
+1 Ronom Unicorn
+1 Valorous Steed
+1 Cloudshift
+1 Inspired Charge
+1 Valorous Stance
+1 Blessed Sanctuary
+1 Faith's Fetters
+1 Thriving Heath
+7 Plains`
+decks["Angels 1"] = `
+1 Angelic Page
+1 Anointed Chorister
+1 Celestial Enforcer
+1 Baneslayer Angel
+1 Emancipation Angel
+1 Serra Angel
+1 Voice of the Provinces
+1 Angelic Ascension
+1 Feat of Resistance
+1 Angelic Edict
+1 Guardian Idol
+1 Scroll of Avacyn
+1 Thriving Heath
+7 Plains`
+decks["Angels 2"] = `
+1 Angelic Arbiter
+1 Angelic Page
+1 Anointed Chorister
+1 Celestial Enforcer
+1 Emancipation Angel
+1 Linvala, Keeper of Silence
+1 Serra Angel
+1 Angelic Ascension
+1 Take Heart
+1 Angelic Edict
+1 Guardian Idol
+1 Scroll of Avacyn
+1 Thriving Heath
+7 Plains`
+decks["Dogs 1"] = `
+1 Affa Guard Hound
+1 Alpine Watchdog
+1 Cathar's Companion
+1 Pack Leader
+1 Rambunctious Mutt
+1 Selfless Savior
+1 Supply Runners 
+1 Trusty Retriever
+1 Feat of Resistance
+1 Release the Dogs 
+1 Secure the Scene
+1 Pacifism
+1 Thriving Heath
+7 Plains`
+decks["Dogs 2"] = `
+1 Affa Guard Hound
+1 Alpine Watchdog
+1 Cathar's Companion
+1 Isamaru, Hound of Konda
+1 Pack Leader
+1 Rambunctious Mutt
+1 Supply Runners 
+1 Trusty Retriever
+1 Dauntless Onslaught
+1 Release the Dogs 
+1 Secure the Scene
+1 Pacifism
+1 Thriving Heath
+7 Plains`
+decks["Enchanted 1"] = `
+1 Anointed Chorister
+1 Blessed Spirits
+1 Knight of the Tusk
+1 Kor Spiritdancer
+1 Stone Haven Pilgrim
+1 Trusty Retriever
+1 Dub
+1 Face of Divinity
+1 Faith's Fetters
+1 Forced Worship
+1 Indomitable Will
+1 Knightly Valor
+1 Thriving Heath
+7 Plains`
+decks["Enchanted 2"] = `
+1 Ajani's Chosen
+1 Blessed Spirits
+1 Bulwark Giant
+1 Staunch Shieldmate
+1 Stone Haven Pilgrim
+1 Trusty Retriever
+1 Celestial Mantle
+1 Face of Divinity
+1 Faith's Fetters
+1 Forced Worship
+1 Indomitable Will
+1 Knightly Valor
+1 Thriving Heath
+7 Plains`
+decks["Doctor 1"] = `
+1 Anointed Chorister
+1 Basri's Acolyte
+1 Brightmare
+1 Bulwark Giant
+1 Mesa Unicorn
+1 Speaker of the Heavens
+1 Revitalize
+1 Swift Response
+1 Take Heart
+1 Faith's Fetters
+1 Griffin Aerie
+1 Light of Promise
+1 Thriving Heath
+7 Plains`
+decks["Doctor 2"] = `
+1 Angel of Mercy
+1 Anointed Chorister
+1 Basri's Acolyte
+1 Brightmare
+1 Speaker of the Heavens
+1 Stone Haven Pilgrim
+1 Moment of Heroism
+1 Revitalize
+1 Swift Response
+1 Secure the Scene
+1 Griffin Aerie
+1 Light of Promise
+1 Thriving Heath
+7 Plains`
+decks["Doctor 3"] = `
+1 Angel of Mercy
+1 Anointed Chorister
+1 Basri's Acolyte
+1 Brightmare
+1 Mesa Unicorn
+1 Rhox Faithmender
+1 Moment of Heroism
+1 Revitalize
+1 Swift Response
+1 Faith's Fetters
+1 Griffin Aerie
+1 Light of Promise
+1 Thriving Heath
+7 Plains`
+decks["Doctor 4"] = `
+1 Alabaster Mage
+1 Angel of Mercy
+1 Anointed Chorister
+1 Basri's Acolyte
+1 Brightmare
+1 Bulwark Giant
+1 Stone Haven Pilgrim
+1 Revitalize
+1 Swift Response
+1 Cradle of Vitality
+1 Griffin Aerie
+1 Path of Bravery
+1 Thriving Heath
+7 Plains`
+decks["Feathered Friends 1"] = `
+1 Aven Gagglemaster
+1 Celestial Enforcer
+1 Concordia Pegasus
+1 Falconer Adept
+1 Gale Swooper
+1 Healer's Hawk
+1 Steel-Plume Marshal
+1 Warded Battlements
+1 Dauntless Onslaught
+1 Moment of Heroism
+1 Aerial Assault
+1 Sky Tether
+1 Thriving Heath
+7 Plains`
+decks["Feathered Friends 2"] = `
+1 Aven Gagglemaster
+1 Celestial Enforcer
+1 Concordia Pegasus
+1 Falconer Adept
+1 Gale Swooper
+1 Healer's Hawk
+1 Steel-Plume Marshal
+1 Warded Battlements
+1 Angelic Ascension
+1 Swift Response
+1 Tandem Tactics
+1 Sky Tether
+1 Thriving Heath
+7 Plains`
+decks["Feathered Friends 3"] = `
+1 Angel of the Dire Hour
+1 Aven Gagglemaster
+1 Celestial Enforcer
+1 Concordia Pegasus
+1 Falconer Adept
+1 Gale Swooper
+1 Healer's Hawk
+1 Warded Battlements
+1 Dauntless Onslaught
+1 Swift Response
+1 Take Heart
+1 Sky Tether
+1 Thriving Heath
+7 Plains`
+decks["Feathered Friends 4"] = `
+1 Archon of Justice
+1 Archon of Redemption
+1 Celestial Enforcer
+1 Concordia Pegasus
+1 Falconer Adept
+1 Gale Swooper
+1 Healer's Hawk
+1 Warded Battlements
+1 Aegis of the Heavens
+1 Dauntless Onslaught
+1 Aerial Assault
+1 Sky Tether
+1 Thriving Heath
+7 Plains`
+decks["Heavily Armored 1"] = `
+1 Basri's Acolyte 
+1 Bulwark Giant 
+1 Lightwalker 
+1 Makeshift Battalion
+1 Patron of the Valiant 
+1 Siege Striker 
+1 Tempered Veteran 
+1 Trusty Retriever
+1 Feat of Resistance
+1 Basri's Solidarity 
+1 Secure the Scene 
+1 Cathars' Crusade
+1 Thriving Heath
+7 Plains`
+decks["Heavily Armored 2"] = `
+1 Basri's Acolyte 
+1 Bulwark Giant 
+1 Lightwalker 
+1 Makeshift Battalion
+1 Patron of the Valiant 
+1 Siege Striker 
+1 Tempered Veteran 
+1 Trusty Retriever
+1 Battlefield Promotion
+1 Gird for Battle
+1 Secure the Scene 
+1 Cathars' Crusade
+1 Thriving Heath
+7 Plains`
+decks["Heavily Armored 3"] = `
+1 Basri's Acolyte 
+1 Bulwark Giant
+1 Lightwalker
+1 Makeshift Battalion
+1 Mikaeus, the Lunarch 
+1 Patron of the Valiant 
+1 Supply Runners 
+1 Tempered Veteran 
+1 Trusty Retriever
+1 Battlefield Promotion 
+1 Divine Arrow
+1 Basri's Solidarity 
+1 Thriving Heath
+7 Plains`
+decks["Heavily Armored 4"] = `
+1 Basri's Acolyte 
+1 Bulwark Giant 
+1 Lightwalker 
+1 High Sentinels of Arashin 
+1 Makeshift Battalion
+1 Siege Striker 
+1 Supply Runners
+1 Tempered Veteran
+1 Trusty Retriever 
+1 Long Road Home
+1 Swift Response 
+1 Duelist's Heritage
+1 Thriving Heath
+7 Plains`
+decks["Legion 1"] = `
+1 Daybreak Charger
+1 Falconer Adept
+1 Knight of the Tusk
+1 Makeshift Battalion
+1 Selfless Savior
+1 Valorous Steed
+1 Inspired Charge
+1 Path to Exile
+1 Raise the Alarm
+1 Basri's Solidarity
+1 Legion's Judgment
+1 Blessed Sanctuary
+1 Thriving Heath
+7 Plains`
+decks["Legion 2"] = `
+1 Daybreak Charger
+1 Inspiring Captain
+1 Makeshift Battalion
+1 Siege Striker
+1 Staunch Shieldmate
+1 Valorous Steed
+1 Raise the Alarm
+1 Basri's Solidarity
+1 Legion's Judgment
+1 Release the Dogs
+1 Glorious Anthem
+1 Faith's Fetters
+1 Thriving Heath
+7 Plains`
+decks["Legion 3"] = `
+1 Daybreak Charger
+1 Makeshift Battalion
+1 Siege Striker
+1 Staunch Shieldmate
+1 Valorous Steed
+1 Raise the Alarm
+1 Fortify
+1 Basri's Solidarity
+1 Legion's Judgment
+1 Release the Dogs
+1 Glorious Anthem
+1 Faith's Fetters
+1 Thriving Heath
+7 Plains`
+decks["Legion 4"] = `
+1 Daybreak Charger
+1 Lena, Selfless Champion
+1 Makeshift Battalion
+1 Mentor of the Meek
+1 Selfless Savior
+1 Siege Striker
+1 Valorous Steed
+1 Raise the Alarm
+1 Fortify
+1 Basri's Solidarity
+1 Legion's Judgment
+1 Faith's Fetters
+1 Thriving Heath
+7 Plains`
